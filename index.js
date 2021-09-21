1f349ad6198c55359df2e311cc75bc2d95a23d9f
Anri <anri0806@hotmail.co.jp>
2021-09-21 16:38:23 -0400

    commit tree 11aab12452bff9e6695af5214109cfd434e99c52

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 // Your code here
+
 const dodger = document.getElementById("dodger");
 
+dodger.style.backgroundColor = "#FF99CC"
+
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  const leftNUmbers = dodger.style.left.replace("px", "")
+  const left = parseInt(leftNUmbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`;
   }
+}
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+})
 
-  function moveDodgerRight(){
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "")
+  const left = parseInt(rightNumbers, 10)
 
-    if (left < 400) {
-        dodger.style.left = `${left + 1}px`;
-  }}
+  if (left < 360) {
+    dodger.style.left = `${left + 5}px`;
+  }
+}
 
-  document.addEventListener("keydown", function(e) {
-      if (e.key === "ArrowRight") {
-          moveDodgerRight();
-      }
-  });
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
